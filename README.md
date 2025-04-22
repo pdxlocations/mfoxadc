2e4b6768bd2ef47d9187fe466cf620e710a1d2c6
pdxlocations <benlipsey@gmail.com>
2025-04-21 18:21:30 -0700

    package with poetry

---
+++ README.md
@@ -0,0 +1,13 @@
+This library provides UDP-based broadcasting of Meshtastic-compatible packets.
+
+# Installation
+
+```bash
+pip install mfoxadc
+```
+
+# Command Line
+
+```bash
+mfoxadc --node-id "!abcd1234"
+```
--- mfoxadc/__main__.py
+++ mfoxadc/__main__.py
@@ -1,14 +1,9 @@
 import time
-import mudp
+import mfoxadc
 import argparse
 
 
-from mudp import (
-    conn,
-    node,
-    send_device_telemetry,
-    send_environment_metrics
-)
+from mfoxadc import conn, node, send_device_telemetry, send_environment_metrics
 
 MCAST_GRP = "224.0.0.69"
 MCAST_PORT = 4403
@@ -18,16 +13,19 @@ OCV = [4190, 4050, 3990, 3890, 3800, 3720, 3630, 3530, 3420, 3300, 3100]
 
 conn.setup_multicast(MCAST_GRP, MCAST_PORT)
 
+
 def read_adc_value(file_path):
     # Read the voltage value from the ADCs
     with open(file_path, "r") as file:
         return file.read().strip()
-    
+
+
 def read_cpu_temperature():
     # Read CPU temperature from the thermal zone
-    with open(TEMP_DIR, 'r') as cpu_temp:
+    with open(TEMP_DIR, "r") as cpu_temp:
         return int(cpu_temp.read()) / 1000
-    
+
+
 def show_percent(voltage):
     for i in range(len(OCV)):
         if OCV[i] <= voltage:
@@ -37,6 +35,7 @@ def show_percent(voltage):
                 return 100.0 / (len(OCV) - 1) * (len(OCV) - 1 - i + (voltage - OCV[i]) / (OCV[i - 1] - OCV[i]))
     return 0
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="mfoxadc: Send ADC and temperature telemetry.")
     parser.add_argument("--node-id", type=str, required=True, help="Node ID (e.g., !596ab32e)")
@@ -45,7 +44,8 @@ def parse_args():
     parser.add_argument("--interval", type=int, default=1800, help="Time between reports in seconds, default is 1800")
     return parser.parse_args()
 
-def main():
+
+def start():
     args = parse_args()
     node.node_id = args.node_id
     node.channel = args.channel
@@ -67,15 +67,16 @@ def main():
 
         voltage = round(IN1_voltage * (5.0 / 1.8), 2)
         battery_level = int(show_percent(voltage * 1000))
-        
+
         send_device_telemetry(voltage=voltage, battery_level=battery_level)
         time.sleep(5)
         send_environment_metrics(temperature=temperature)
 
         time.sleep(interval)
 
+
 if __name__ == "__main__":
     try:
-        main()
+        start()
     except KeyboardInterrupt:
         pass
